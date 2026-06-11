ec92ff3af2212d238feaa3769dac0c3997eca6d7
Aditya Sarkar <104256382+awdtyo@users.noreply.github.com>
2026-06-11 11:17:52 +0530

    Update worker.py

--- backend/worker.py
+++ backend/worker.py
@@ -1,4 +1,4 @@
 from app.core.celery_app import celery_app
 
-# Expose celery app for: celery -A worker.celery_app worker --loglevel=info
+# Expose celery app for: celery -A worker.celery_app worker --loglevel=info 
 
